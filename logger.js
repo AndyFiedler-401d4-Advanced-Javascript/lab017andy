1d5c531afb0c130d0d12716dc4c117dc21a026c9
Andy Fiedler <andyfidd@gmail.com>
2019-10-08 16:53:34 -0500

    Banjo added demo code and stuff

---
+++ logger.js
@@ -0,0 +1,19 @@
+"use strict"; 
+
+const eventHub = require('./hub');
+const net = require('net');
+const client = new net.Socket();
+
+const PORT = process.env.PORT || 3001;
+
+client.connect(PORT, 'localhost', () => {
+
+})
+
+
+eventHub.on('save', file => {
+  console.log(`Record ${file} was saved!`)});
+
+eventHub.on('error', file => {
+  console.error('Something went wrong.')}
+  );
---
+++ src/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+//do i even need the hub import??
+// const hub = require('./hub');
+
+
+const fs = require('fs');
+const uuid = require('uuid');
+
+require('../logger');
+require('./network-logger');
+require('./cache-invalidator');
+
+
+const eventHub = require('./event');
+const {promisify} = require('util');
+
+const readFileProm = promisify(fs.readFile);
+
+const writeFileProm = promisify(fs.writeFile);
+
+
+const alterFile = (file) => {
+  readFileProm(file)
+  .then(data => {
+    let text = data.toString().toUpperCase();
+    writeFileProm(file, Buffer.from(text));
+    console.log(text);
+      console.log(`${file} saved`);
+      eventHub.emit('save', file);
+  })
+   .catch(error => {
+     eventHub.emit('error', error)
+   })
+  };
+
+
+  // Don't save until we're probably connected
+setInterval(() => {
+  saveToDb({ name: uuid() });
+}, 500);
+
+const { saveToDb } = require('./db');
+
+let file = process.argv.slice(2).shift();
+alterFile(file);
